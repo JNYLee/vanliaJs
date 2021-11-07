df867be80a3da89714a23e15b441fbbc6fcb057b
JNYLee <junyjuny1021@gmail.com>
2021-11-07 22:14:12 +0900

    [loginr/init]: 링크 기본동작 막기

--- LOGIN/app.js
+++ LOGIN/app.js
@@ -1,8 +1,18 @@
+const logInInForm = document.querySelector("#login-form");
 const logInInput = document.querySelector("#login-form input");
-const logInButton = document.querySelector("#login-form button");
 
-function onLogInBtnClick() {
-    console.log("hello", logInInput.value);
+const link = document.querySelector("a");
+function onLogInSubmit(event) {
+    event.preventDefault();
+    const username = logInInput.value;
+    console.log(username);
 }
 
-logInButton.addEventListener("click", onLogInBtnClick);
+function handleLinkClick (event) {
+    event.preventDefault()
+    console.log(event);
+    alert("Clicked!");
+}
+
+logInInForm.addEventListener("submit", onLogInSubmit);
+link.addEventListener("click", handleLinkClick);
