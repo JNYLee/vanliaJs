800cf6dcdfb2fb7a2bc8c266547d821df4bdba72
JNYLee <junyjuny1021@gmail.com>
2021-11-07 17:30:23 +0900

    [calculator/init]: 계산기만들기

---
+++ WELCOME TO JAVASCRIPT/calculator/app.js
@@ -0,0 +1,17 @@
+const calculator = {
+    add: function (firstNumber, secondNumber) {
+        console.log(firstNumber + secondNumber);
+    },
+    min: function (firstNumber, secondNumber) {
+        console.log(firstNumber - secondNumber);
+    },
+    div: function (firstNumber, secondNumber) {
+        console.log(firstNumber / secondNumber);
+    },
+    mul: function (firstNumber, secondNumber) {
+        console.log(firstNumber * secondNumber);
+    },
+    powerOf: function (firstNumber, secondNumber) {
+        console.log(firstNumber ** secondNumber);
+    },
+}
