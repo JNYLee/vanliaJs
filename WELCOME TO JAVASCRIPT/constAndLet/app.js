6ce42dacff92da05ba9a6c734aa4476bf6da5fd8
JNYLee <junyjuny1021@gmail.com>
2021-11-07 16:38:23 +0900

    [constAndLet/init]: const와 let 객체 만들어보기

---
+++ WELCOME TO JAVASCRIPT/constAndLet/app.js
@@ -0,0 +1,14 @@
+const a = 5;
+const b = 2;
+
+let myName = "nico";
+
+console.log(a + 2);
+console.log(a * 2);
+console.log(a / 2);
+
+console.log("hello"+myName);
+
+myName = "nicolas";
+
+console.log("hello"+myName);
