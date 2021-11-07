749fcfab00138f514d220bebe75d3014e38629b6
JNYLee <junyjuny1021@gmail.com>
2021-11-07 17:51:47 +0900

    [conditional/init]: if함수로 숫자인지 확인

--- WELCOME TO JAVASCRIPT/conditional/app.js
+++ WELCOME TO JAVASCRIPT/conditional/app.js
@@ -6,3 +6,12 @@ console.log(parseInt(age));
  // 변수의 타입을 보는 방법
  console.log(typeof age);
 
+// NaN인지 확인하는 방법
+isNaN(age);
+
+// 숫자 입력 확인
+if(isNaN(age)) {
+    console.log("Please input Number");
+} else {
+    console.log("Thanks you for writing your age.");
+}
